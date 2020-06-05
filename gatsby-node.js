a1678a5b622e19d939ecd143b91ba5f743ba78aa
Mathias <mathias@grahlie.se>
2020-06-05 22:10:03 +0200

    cleaned gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,10 @@
 const fs = require("fs")
 
 exports.onPreBootstrap = ({ reporter }) => {
-  const contentPath = "./src/images"
+  const contentPath = "src/images"
 
   if (!fs.existsSync(contentPath)) {
     reporter.info(`creating the ${contentPath} directory`)
     fs.mkdirSync(contentPath)
-    //fs.copyFile('./src/images/logotype.png', `${contentPath}/logotype.png`)
-    //fs.copyFile('./src/images/favicon.png', `${contentPath}/favicon.png`)
   }
 }
