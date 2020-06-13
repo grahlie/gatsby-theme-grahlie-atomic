798e200c45a927b3609ff5c1e55e0b90a3754fb3
Mathias <mathias@grahlie.se>
2020-06-13 12:40:25 +0200

    Updated gatsby-node to remove erros when building

---
+++ content/pages/about.md
@@ -0,0 +1,6 @@
+---
+path: '/about'
+date: '2020/01/01'
+title: 'Hello World'
+summary: 'hello world post'
+---
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,9 +4,13 @@ exports.onPreBootstrap = ({ reporter }) => {
   const srcFolder = 'src'
   const imageFolder = "images"
 
-  if (!fs.existsSync(imageFolder)) {
-    reporter.info(`creating the ${srcFolder} & ${srcFolder}/${imageFolder} directory`)
+  if (!fs.existsSync(srcFolder)) {
+    reporter.info(`creating the ${srcFolder} directory`)
     fs.mkdirSync(srcFolder)
+  }
+  
+  if (!fs.existsSync(`${srcFolder}/${imageFolder}`)) {
+    reporter.info(`creating the ${srcFolder}/${imageFolder} directory`)
     fs.mkdirSync(`${srcFolder}/${imageFolder}`)
   }
 }
